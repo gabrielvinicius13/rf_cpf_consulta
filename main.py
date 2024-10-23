7df8b10e731fa63a56d6f9770a02326dc09764a6
gabrielvinicius13 <gabriel.silva@cnseg.org.br>
2024-10-23 15:43:20 -0300

    adicionado loop para tentar novamente em caso de erro

--- main.py
+++ main.py
@@ -16,54 +16,60 @@ download_dir = 'consultas'
 os.makedirs(download_dir, exist_ok=True)
 
 def realizar_consulta(cpf, data_nascimento):
-    
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+    while True:  # Loop para tentar novamente em caso de erro
+        try:
 
-        url = "https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp"
-        page.goto(url)
+            with sync_playwright() as p:
+                browser = p.chromium.launch(headless=False)
+                page = browser.new_page()
 
-        page.wait_for_selector('input[name="txtCPF"]', timeout=10000)
+                url = "https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp"
+                page.goto(url)
 
-        page.fill('input[name="txtCPF"]', str(cpf))
-        page.fill('input[name="txtDataNascimento"]', str(data_nascimento))
+                page.wait_for_selector('input[name="txtCPF"]', timeout=10000)
 
-        # Localizar o checkbox do captcha
-        hcaptcha_frame = page.frame_locator("iframe[title='Widget contendo caixa de seleção para desafio de segurança hCaptcha']")
-        checkbox = hcaptcha_frame.locator('div[role="checkbox"]')
-        checkbox.click()
-        print("Clicando no captcha....")
+                page.fill('input[name="txtCPF"]', str(cpf))
+                page.fill('input[name="txtDataNascimento"]', str(data_nascimento))
+
+                # Localizar o checkbox do captcha
+                hcaptcha_frame = page.frame_locator("iframe[title='Widget contendo caixa de seleção para desafio de segurança hCaptcha']")
+                checkbox = hcaptcha_frame.locator('div[role="checkbox"]')
+                checkbox.click()
+                print("Clicando no captcha....")
+                
+                # page.wait_for_timeout(600)
+                page.wait_for_function("""
+                    () => document.querySelector("[name='h-captcha-response']").value !== ''
+                """, timeout=10000)           
+
+                # Submeter o formulário e esperar a navegação
+                page.click('input[name="Enviar"]')
+                page.wait_for_selector('div[class="clConteudoEsquerda"]', timeout=10000)
+                print("Conteudo encontrado")
+
+                # Capturar o conteúdo da página de resposta e usar BeautifulSoup para processar
+                response_html = page.content()
+                soup = BeautifulSoup(response_html, 'html.parser')
+                conteudos_esquerda = soup.find_all('div', class_='clConteudoEsquerda')
+
+                file_name = f"{cpf}.txt"
+                file_path = os.path.join(download_dir, file_name)
+
+                with open(file_path, "w", encoding="utf-8") as file:
+                    for div in conteudos_esquerda:
+                        bold_tags = div.find_all('b')
+                        for bold in bold_tags:
+                            file.write(bold.get_text(strip=True) + '\n')
+
+                print(f"Consulta para CPF {cpf} concluída. Conteúdo salvo em {file_path}")
+
+                browser.close()
+                
+                return
         
-        # page.wait_for_timeout(600)
-        page.wait_for_function("""
-            () => document.querySelector("[name='h-captcha-response']").value !== ''
-        """, timeout=10000)           
-
-        # Submeter o formulário e esperar a navegação
-        page.click('input[name="Enviar"]')
-        page.wait_for_selector('div[class="clConteudoEsquerda"]', timeout=10000)
-        print("Conteudo encontrado")
-
-        # Capturar o conteúdo da página de resposta e usar BeautifulSoup para processar
-        response_html = page.content()
-        soup = BeautifulSoup(response_html, 'html.parser')
-        conteudos_esquerda = soup.find_all('div', class_='clConteudoEsquerda')
-
-        file_name = f"{cpf}.txt"
-        file_path = os.path.join(download_dir, file_name)
-
-        with open(file_path, "w", encoding="utf-8") as file:
-            for div in conteudos_esquerda:
-                bold_tags = div.find_all('b')
-                for bold in bold_tags:
-                    file.write(bold.get_text(strip=True) + '\n')
-
-        print(f"Consulta para CPF {cpf} concluída. Conteúdo salvo em {file_path}")
-
-        # Fechar o navegador
-        browser.close()
-
+        except Exception as e:
+            print(f"Erro ao capturar fazer a consulta {e}. Tentando novamente...")
+    
 # Iterar sobre o DataFrame e realizar consultas para cada CPF
 for index, row in df.iterrows():
     cpf = row['CPF']
